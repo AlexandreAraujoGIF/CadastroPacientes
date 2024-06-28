df847dc35963795e715c0af024d6b9fd91302d58
AlexandreAraujoGIF <124702787+AlexandreAraujoGIF@users.noreply.github.com>
2024-06-28 15:19:48 -0300

    Update cadastro.swift
    Adicionado um loop infinito com while para que o sistema finalize apenas quando o usuário digitar 6

--- cadastro.swift
+++ cadastro.swift
@@ -1,5 +1,3 @@
-// Versão 0.5
-
 // Bibliotecas e funções
 import Foundation
 
@@ -14,74 +12,81 @@ struct Paciente {
     var notasObservacoes: String
 }
 var pacientes: [Paciente] = [] // Vetor para armazenar os pacientes
+var verifica: Int = 0
 
 // Mensagem inicial
 print("Seja bem vindo ao Hospital João Trici")
 print()
-print("As funcoes disponiveis são:")
-    print("Digite o codigo correspondente a funcao que deseja")
+    print("Digite o codigo correspondente a funcao que deseja!")
     print("1. Cadastrar Paciente: Adicionar novos pacientes ao sistema.")
     print("2. Alterar Paciente: Atualizar informações de pacientes existentes.")
     print("3. Excluir Paciente: Remover pacientes do sistema.")
     print("4. Visualizar Pacientes: Listar e visualizar informações dos pacientes cadastrados.")
     print("5. Pesquisar Paciente: Buscar pacientes por nome ou número de identificação.")
+    print("6. Fechar o Sistema.")
 print()
-print("O que voce deseja fazer")
+print("O que voce deseja fazer ?")
 
-// Verificando se o usuário colocou uma opção valida
-if let funcao = Int(readLine()!) {
-    // As funções do sistema
-    switch funcao {
-    case 0: 
-        print("Teste")
-    case 1: // Cadastrando o Paciente
-        print("Cadastrar paciente")
+
+while verifica != 6{ // Loop "infinito"
+    // Verificando se o usuário colocou uma opção valida
+    if let funcao = Int(readLine()!) {
+        // As funções do sistema
+        switch funcao {
+        case 0: 
+            print("Teste")
+        case 1: // Cadastrando o Paciente
+            print("Cadastrar paciente")
+            
+            // Loop para capturar dados dos pacientes
+            for _ in 0..<1 { // Adicionando dados de 1 pacientes, podendo alterar a quantidade conforme necessário
+            
+            print("Digite o nome completo do paciente:")
+            let nomeCompleto = readLine()!
+            
+            print("Digite a data de nascimento do paciente:")
+            let dataNascimento = readLine()!
+            
+            print("Digite o número de identificação do paciente:")
+            let identificacao = readLine()!
+            
+            print("Digite o diagnóstico médico do paciente:")
+            let diagnostico = readLine()!
+            
+            print("Digite as medicações em uso pelo paciente:")
+            let medicacoesEmUso = readLine()!
+            
+            print("Digite a data de admissão do paciente:")
+            let dataAdmissao = readLine()!
+            
+            print("Digite as notas e observações dos médicos sobre o paciente:")
+            let notasObservacoes = readLine()!
         
-        // Loop para capturar dados dos pacientes
-        for _ in 0..<1 { // Adicionando dados de 1 pacientes, podendo alterar a quantidade conforme necessário
+            // Criando um objeto Paciente com os dados fornecidos e adicionando ao vetor
+            let paciente = Paciente(nomeCompleto: nomeCompleto, dataNascimento: dataNascimento, identificacao: identificacao, diagnostico: diagnostico, medicacoesEmUso: medicacoesEmUso, dataAdmissao: dataAdmissao, notasObservacoes: notasObservacoes)
         
-        print("Digite o nome completo do paciente:")
-        let nomeCompleto = readLine()!
+            pacientes.append(paciente)
+            }
+        case 2: // Alterando dados do Paciente
+            print("Alterar dados do Paciente")
         
-        print("Digite a data de nascimento do paciente:")
-        let dataNascimento = readLine()!
+        case 3: // Excluindo um Paciente
+            print("Excluir um Paciente")
+            
+        case 4: // Visualizando Pacientes
+            print("Visualizar Pacientes")
         
-        print("Digite o número de identificação do paciente:")
-        let identificacao = readLine()!
-        
-        print("Digite o diagnóstico médico do paciente:")
-        let diagnostico = readLine()!
-        
-        print("Digite as medicações em uso pelo paciente:")
-        let medicacoesEmUso = readLine()!
-        
-        print("Digite a data de admissão do paciente:")
-        let dataAdmissao = readLine()!
-        
-        print("Digite as notas e observações dos médicos sobre o paciente:")
-        let notasObservacoes = readLine()!
+        case 5: // Pesquisando Paciente
+            print("Pesquisar Paciente")
     
-        // Criando um objeto Paciente com os dados fornecidos e adicionando ao vetor
-        let paciente = Paciente(nomeCompleto: nomeCompleto, dataNascimento: dataNascimento, identificacao: identificacao, diagnostico: diagnostico, medicacoesEmUso: medicacoesEmUso, dataAdmissao: dataAdmissao, notasObservacoes: notasObservacoes)
-    
-        pacientes.append(paciente)
+        case 6: // Fechar Sistema
+            print("Obrigado por usar o sistema")
+            verifica = 6
+            break
+        default:
+            print("Por favor, escolha uma das opções disponíveis")
         }
-    case 2: // Alterando dados do Paciente
-        print("Alterar dados do Paciente")
-    
-    case 3: // Excluindo um Paciente
-        print("Excluir um Paciente")
-        
-    case 4: // Visualizando Pacientes
-        print("Visualizar Pacientes")
-    
-    case 5: // Pesquisando Paciente
-        print("Pesquisar Paciente")
-    
-    default:
-        print("Por favor, escolha uma das opções disponíveis")
+    }else {
+        print("Opção invalida. Tente novamente")
     }
-}else {
-    print("Opção invalida")
 }
-// Fim :)
